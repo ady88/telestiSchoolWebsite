dc2ab1ffb28b545031cef286c6ef8b271dae6409
Adrian <ady8_8@yahoo.com>
2015-11-14 14:52:18 +0200

    removed basic auth from spring

---
+++ src/main/java/home/telestischool/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package home.telestischool;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+/**
+ *
+ * @author adrian
+ */
+@Configuration
+@EnableWebMvcSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home", "/events", "/history", "/contact",
+                        "/resources/style.css",
+                        "/webjars/materializecss/0.97.0/**",
+                        "/webjars/jquery/2.1.4/jquery.js")
+                .permitAll().anyRequest()
+                .authenticated();
+    }
+}
